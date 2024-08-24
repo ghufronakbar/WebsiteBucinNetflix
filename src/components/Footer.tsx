e1747f4329a6089a973223b71f651e316c15fde2
ghufron akbar <alan.040219.gam@gmail.com>
2024-08-24 20:51:22 +0700

    update cr

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-black text-white h-44 flex items-center justify-center">
-      <p className="text-sm">&copy; 2023 - 2024, Goddess Rockstar 2112</p>
+      <p className="text-sm">&copy; 2024 | Lans The Prodigy</p>
     </footer>
   );
 };
